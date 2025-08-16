ec93c28e2380410bc7ff850c85c0733074e52b62
FrankLoSt <phananhnam1998@gmail.com>
2025-08-16 09:23:51 +0700

    Roll dice

--- app/src/main/java/com/example/testingground/MainActivity.kt
+++ app/src/main/java/com/example/testingground/MainActivity.kt
@@ -97,32 +97,28 @@ private fun ArticleCard(
 
 ) {
     Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-            Image(
-                painter = painterResource(R.drawable.bg_compose_background),
-                contentDescription = null,
-                modifier = Modifier.fillMaxWidth()
-            )
-            Text(
-                text = stringResource(R.string.jetpack_compose_tutorial),
-                fontSize = 24.sp,
-                modifier = Modifier.padding(16.dp),
-                textAlign = TextAlign.Justify
-            )
-            Text(
-                text = stringResource(R.string.jetpack_compose_is_a_modern_toolkit_for_building_native_android_ui_compose_simplifies_and_accelerates_ui_development_on_android_with_less_code_powerful_tools_and_intuitive_kotlin_apis),
-                modifier = Modifier
-                    .padding(start = 16.dp, end = 16.dp),
-                textAlign = TextAlign.Justify
-            )
-           Text(
-                text = stringResource(R.string.in_this_tutorial_you_build_a_simple_ui_component_with_declarative_functions_you_call_compose_functions_to_say_what_elements_you_want_and_the_compose_compiler_does_the_rest_compose_is_built_around_composable_functions_these_functions_let_you_define_your_app_s_ui_programmatically_because_they_let_you_describe_how_it_should_look_and_provide_data_dependencies_rather_than_focus_on_the_process_of_the_ui_s_construction_such_as_initializing_an_element_and_then_attaching_it_to_a_parent_to_create_a_composable_function_you_add_the_composable_annotation_to_the_function_name),
-               modifier = Modifier.padding(16.dp),
-               textAlign = TextAlign.Justify
-           )
-
-    }
+         modifier = Modifier
+             .fillMaxSize(),
+         verticalArrangement = Arrangement.Center,
+         horizontalAlignment = Alignment.CenterHorizontally
+     ) {
+        Image (
+         painter = when (viewModel1.randomDice) {
+             1 -> painterResource(R.drawable.dice_1)
+             2 -> painterResource(R.drawable.dice_2)
+             3 -> painterResource(R.drawable.dice_3)
+             4 -> painterResource(R.drawable.dice_4)
+             5 -> painterResource(R.drawable.dice_5)
+             else -> painterResource(R.drawable.dice_6)
+         },
+            contentDescription = null
+        )
+         Button(
+             onClick = {viewModel1.rollDice()}
+         ) {
+             Text(stringResource(R.string.roll))
+         }
+     }
 }
 
 
